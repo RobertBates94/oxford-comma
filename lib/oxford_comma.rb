1e4174ddce65619594186d19e1f91c8ffd2ec00e
RobertBates94 <bates.to.infinity@gmail.com>
2021-03-02 20:44:02 -0600

    commit tree 4b4ab81017b0774b8a9f758cd6d8164d1292f11e

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
-def oxford_comma(array)
+require 'pry'
 
+
+def oxford_comma(array)
+    last_word = array[-1]
+  if array.size == 1
+    return array.join
+  elsif array.size == 2
+    return array.join" and "
+  elsif array.size >= 3
+    array.pop
+    array << "and "
+    return array.join(", ") << "#{last_word}"
+  end
 end
+  
+
+
+
